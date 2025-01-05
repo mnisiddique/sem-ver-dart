d29719fab457d2979159add1f1f140643bced771
prince-ael <mnisiddique@ael-bd.com>
2025-01-05 17:51:30 +0600

    feat: removes build number interface

--- gen/bin/gen.dart
+++ gen/bin/gen.dart
@@ -1,5 +1,3 @@
-import 'package:gen/build_number/build_number.dart';
-import 'package:gen/build_number/project_settings.dart';
 import 'package:gen/command/command.dart';
 import 'package:gen/version/commit_source.dart';
 
@@ -7,14 +5,6 @@ import 'package:gen/version/version_calculator.dart';
 import 'package:gen/release_note/release_note_gen.dart';
 
 void main(List<String> arguments) async {
-  if (arguments.first.trim().contains("build_number")) {
-    final buildNumber = FirebaseBuildNumber(
-      settings: await ProjectSettings.read(),
-      params: arguments,
-    );
-    print(await buildNumber.get());
-    return;
-  }
   final tag = await latestTag;
   final commitSrc = CommitSource(tag: tag);
   final commits = await commitSrc.getCommits();
@@ -23,7 +13,7 @@ void main(List<String> arguments) async {
     print(await calculator.calculate());
   } else if (arguments.first.trim() == 'release-note') {
     final releaseNote = ReleaseNoteGen(commits: commits);
-    print(releaseNote.genReleaseNote());
+    print(releaseNote.genReleaseNote(commitSrc.branchName));
   } else {
     print('only sem-ver, build, release-note are valid parameters');
   }
