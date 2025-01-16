0ba4d9c90c10ad5fa7e3d694711bf86b539ed861
prince-ael <mnisiddique@ael-bd.com>
2025-01-16 21:24:16 +0600

    feat: adds tag verification

--- gen/bin/gen.dart
+++ gen/bin/gen.dart
@@ -6,25 +6,55 @@ import 'package:gen/version/version_calculator.dart';
 import 'package:gen/release_note/release_note_gen.dart';
 
 void main(List<String> arguments) async {
-  final tag = await latestTag;
-  final commitSrc = CommitSource(tag: tag);
-  final commits = await commitSrc.getCommits();
   if (arguments.first.trim() == 'sem-ver') {
-    final calculator = VersionCalculator(tag: tag, commits: commits);
-    print(await calculator.calculate());
+    calculateSemVer(arguments);
   } else if (arguments.first.trim() == 'release-note') {
-    final releaseNote = ReleaseNoteGen(commits: commits);
-    print(releaseNote.genReleaseNote(commitSrc.branchName));
+    getReleaseNote(arguments);
   } else if (arguments.first.trim() == 'clean') {
     await cleaner.clean(arguments);
+  } else if (arguments.first.trim() == 'verify-tag') {
+    verifyTag(arguments);
   } else {
     print('only sem-ver, build, release-note are valid parameters');
   }
 }
 
-Future<String?> get latestTag async {
+void getReleaseNote(List<String> arguments) async {
+  final tag = await latestTag(arguments.length > 1 ? arguments[1] : null);
+  final commitSrc = CommitSource(tag: tag);
+  final commits = await commitSrc.getCommits();
+  final releaseNote = ReleaseNoteGen(commits: commits);
+  print(releaseNote.genReleaseNote(commitSrc.branchName));
+}
+
+void calculateSemVer(List<String> arguments) async {
+  final tag = await latestTag(arguments.length > 1 ? arguments[1] : null);
+  final commitSrc = CommitSource(tag: tag);
+  final commits = await commitSrc.getCommits();
+  final calculator = VersionCalculator(tag: tag, commits: commits);
+  print(await calculator.calculate());
+}
+
+void verifyTag(List<String> arguments) async {
+  const red = '\x1B[31m';
+  const yellow = '\x1B[33m';
+  const reset = '\x1B[0m';
+  if (arguments.length < 2) {
+    throw ArgumentError("$red No tag is given for verification $reset");
+  }
+  final tag = await latestTag(arguments.length > 2 ? arguments[2] : null);
+  if (tag != null && !tag.contains(arguments[1])) {
+    print("$yellow latest tag: $tag$reset");
+    throw "$red Repository must have to be tagged with last released version. Either ${arguments[1]} is not latest or doesn't exist$reset";
+  }
+}
+
+Future<String?> latestTag(String? prefix) async {
   try {
-    return await kGetLatestTagFromMain.run();
+    final cmd = prefix == null
+        ? kGetLatestTagFromMain
+        : getLatestTagByMatchingPrefix("'$prefix*'");
+    return await cmd.run();
   } catch (e) {
     return null;
   }
--- gen/lib/command/command_source.dart
+++ gen/lib/command/command_source.dart
@@ -3,10 +3,15 @@ part of 'command.dart';
 const kGetBranchName = Command(cmd: 'git rev-parse --abbrev-ref HEAD');
 
 const kGetLatestTagFromMain =
-    Command(cmd: 'git describe --tags --abbrev=0 origin/main');
+    Command(cmd: 'git tag --list --merged main | sort -V | tail -n 1');
 
 Command getCommitAfter(String start) {
   return Command(cmd: 'git log --oneline ${start.trim()}..HEAD');
+}
+
+Command getLatestTagByMatchingPrefix(String prefix) {
+  return Command(
+      cmd: 'git tag --list $prefix --merged main | sort -V | tail -n 1');
 }
 
 Command getCommitFromBranch(String branchName) {
